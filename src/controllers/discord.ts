79ec4e5310849a438929dd1934c90d94124ee8b1
tojurnru <thisisexactlymyemail@gmail.com>
2021-10-24 16:49:26 +0800

    ADD initial source code

---
+++ src/controllers/discord.ts
@@ -0,0 +1,30 @@
+import path from 'path';
+import { Client, Intents } from 'discord.js';
+import { Message as DiscordMessage } from 'discord.js';
+
+import logger from './logger';
+// import { handleMessage } from './message';
+
+const filename = path.basename(__filename);
+
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
+
+client
+  .on('ready', () => {
+    logger.info(`logged in as ${client.user?.tag}`);
+  })
+  .on('debug', (info) => {
+    logger.silly(`${filename} | debug: ${info}`);
+  })
+  // .on('messageCreate', handleMessage)
+  .on('messageCreate', (message: DiscordMessage) => {
+    console.log(message.content);
+  })
+  .on('error', (err) => {
+    logger.error(`${filename} | ${err}`);
+    logger.error(err);
+  });
+
+export default client;
