daf1eff1145e72f42c54275065dec51b5dbb704f
tojurnru <thisisexactlymyemail@gmail.com>
2021-12-29 11:26:36 +0800

    REMOVE the word "original message" when making a repost on their application form

--- src/parser/parseThreadMessage.ts
+++ src/parser/parseThreadMessage.ts
@@ -7,9 +7,7 @@ export const parseThreadMessage = async (
 ): Promise<string> => {
   // const { guildId, channelId, id } = message;
   // output += `<https://discord.com/channels/${guildId}/${channelId}/${id}>\n`;
+  // const prefix = 'Original Message:\n';
 
-  const prefix = 'Original Message:\n';
-  const trimMessage = message.content.substr(0, 2000 - prefix.length);
-
-  return prefix + trimMessage;
+  return message.content.substr(0, 2000);
 };
